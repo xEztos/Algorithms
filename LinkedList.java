bfee2a0524c0f9fbcb67a67940c3646fab50c6db
xEztos <alex_s_2208@yahoo.com>
2017-12-25 16:57:27 -0600

    Moved ListNode into its own file space

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,6 @@
 public class LinkedList{
 	private ListNode firstNode;
+	private int length
 
 	public LinkedList(ListNode firstNode){
 		this.firstNode = firstNode;
@@ -22,6 +23,7 @@ public class LinkedList{
 			}
 			temp.setNext(node);
 		}
+		length++;
 	}
 
 	/**
@@ -46,6 +48,7 @@ public class LinkedList{
 		if(firstNode.equals(node)){
 			ListNode ret = firstNode;
 			firstNode = firstNode.getNextNode();
+			length--;
 			return ret;
 		}
 
@@ -54,6 +57,8 @@ public class LinkedList{
 			if(temp.getNextNode().equals(node)){
 				ListNode ret = temp.getNextNode();
 				temp.setNext(ret.getNextNode());
+				length--;
+				return ret;
 			}
 		}
 
@@ -95,6 +100,21 @@ public class LinkedList{
 	}
 
 	/**
+	 *	returns the length of the list
+	 *	@return the length of the list
+	 */
+	public int length(){
+		int length = 0;
+		ListNode temp = firstNode;
+		while(temp!=null){
+			count++;
+			temp = temp.getNextNode();
+		}
+
+		return length;
+	}
+
+	/**
 	 *	gets a specified node contained within this list, or null otherwise
 	 *	@return the specified node contained within this list, or null if it doesn't exist
 	 */
@@ -131,138 +151,5 @@ public class LinkedList{
 			ret += temp.toString() + " | ";
 		}
 		ret += temp.toString() + " }";
-	}
-}
-
-
-class ListNode{
-	private int cost;
-	private int distance;
-	private int id;
-	private ListNode nextNode;
-
-
-	/**
-	 *  ListNode constructor that takes in and assigns
-	 *  the location ID and the next next node in the linked list.
-	 *  @param id the location idenftifier
-	 *	@param distance the distance of this location
-	 *	@param cost the cost to get to this node
-	 *  @param nextNode the next ListNode in the series
-	 */
-	public ListNode(int id, int distance, int cost, ListNode nextNode){
-		this.id = id;
-		this.nextNode = nextNode;
-		this.distance = distance;
-	}
-
-	/**
-	 *	ListNode constructor that takes in and assigns a location ID, distance, and cost
-	 *	@param id the location identifier
-	 *	@param distance the distance of this location
-	 *	@param cost the cost to get to this node
-	 */
-	public ListNode(int id, int distance, int cost){
-		this(id, distance, cost, null);
-	}
-
-	/**
-	 *	Extended constructor that only takes in a location identifier and distance.
-	 *  @param id the location identifier number
-	 *	@param distance the distance of this location
-	 */
-	public ListNode(int id, int distance){
-		this(id, distance, distance, null);
-	}
-
-	/**
-	 *	creates a new ListNode with a specified identifier and a near infinite distance and cost
-	 *	@param id the location ID of the node
-	 */
-	public ListNode(int id){
-		this(id, Integer.MAX_VALUE, Integer.MAX_VALUE, null);
-	}
-
-	/**
-	 *	enables the rewrite of value of cost after instantiation
-	 *	@param cost the new cost it takes to get to this location
-	 */
-	public void setCost(int cost){
-		this.cost = cost;
-	}
-
-	/**
-	 *	enables the rewrite of value of distance after instantiation
-	 *	@param distance the new distance this node is
-	 */
-	public void setDistance(int distance){
-		this.distance = distance;
-	}
-
-	/**
-	 *	reassociates the next node in the list to a new value
-	 *	@param nextNode the next node to assign in the list
-	 */
-	public void setNext(ListNode nextNode){
-		this.nextNode = nextNode;
-	}
-
-	/**
-	 *	Returns the cost needed to get to this location
-	 *	@return the cost needed to get to this location
-	 */
-	public int getCost(){
-		return this.cost;
-	}
-
-	/**
-	 *	returns the distance to this location
-	 *	@return the distance to this location
-	 */
-	public int getDistance(){
-		return this.distance;
-	}
-
-	/**
-	 *	returns the location identifier number
-	 *  @return the location identifier number
-	 */
-	public int getID(){
-		return this.id;
-	}
-
-	/**
-	 *	returns the next node in the list
-	 *	@return the next node in the list
-	 */
-	public ListNode getNextNode(){
-		return this.nextNode;
-	}
-
-	/**
-	 * 	returns true if the given node is equal to this node
-	 *	@return true if the given node is equal to this node
-	 */
-	public boolean equals(ListNode node){
-		if(node == null) return false;		// why would you even do this to me
-		if(this.id != node.getID()) return false;
-		// if(this.distance != node.getDistance()) return false;
-		// if(this.cost != node.getCost()) return false;
-		// if(!this.nextNode.equals(node.nextNode())) return false;
-		return true;
-	}
-
-	/**
-	 *	returns a string representation of this node:
-	 *		"Node ID: <int>, Distance: <int>, Cost: <int>"
-	 *	@return a string representation of this node
-	 */
-	public String toString(){
-		String ret = "NodeID: " + this.id;
-		String ret = ret + ",  Distance: " + this.distance;
-		String ret = ret + ",  Cost: " + this.cost;
-		// String ret = ret + ",  " + this.nextNode;
-
-		return ret;
 	}
 }
