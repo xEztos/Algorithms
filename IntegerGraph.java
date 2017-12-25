7693316edb158dc0ea366f18c73a6faaf7ad602b
xEztos <alex_s_2208@yahoo.com>
2017-12-25 16:58:54 -0600

    Added some getters and setters

--- IntegerGraph.java
+++ IntegerGraph.java
@@ -1,5 +1,5 @@
 public class IntegerGraph{
-	LinkedList[] graph;
+	private LinkedList[] graph;
 
 	/**
 	 *	creates an empty graph with a specified number of nodes
@@ -23,10 +23,33 @@ public class IntegerGraph{
 	 *	@return true if the edge was successfuly added or false if an edge already exists
 	 */
 	public boolean addEdge(int nodeA, int nodeB, int distance, int cost){
-
+		graph[nodeA].addNode(nodeB, distance, cost);
 	}
 
+	/**
+	 *  Returns the number of vertexes in the graph
+	 *  @return the number of vertexes in the graph
+	 */
+	public int getNumVertexes(){
+		return graph.length;
+	}
 
+	/**
+	 *	returns the number of one-way edges in the graph
+	 *	@return the number of one-way edges in the graph
+	 */
+	public int getNumEdges(){
+		int count = 0;
+		for(int i = 0; i < graph.length; i++){
+			count += graph[i].length();
+		}
+		return count;
+	}
+
+	/**
+	 *	Uses the ____ algorithm to determine if the graph has infinite negative cycles
+	 *	@return true if a negative cycle exists and false if a negative cycle does not exisit
+	 */
 	public boolean hasCycles(){
 		return true;
 	}
